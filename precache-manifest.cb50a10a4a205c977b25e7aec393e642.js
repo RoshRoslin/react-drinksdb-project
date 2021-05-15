a0d60245abdf7103d403a3000f72cf22ec5c4943
RoshRoslin <RoshRoslin@gmail.com>
2021-05-15 14:56:06 -0700

    Updates

---
+++ precache-manifest.cb50a10a4a205c977b25e7aec393e642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de995624557b66acf5908105e8a64c0b",
+    "url": "/react-drinksdb-project/index.html"
+  },
+  {
+    "revision": "11d315341d0124e2b615",
+    "url": "/react-drinksdb-project/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/react-drinksdb-project/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-drinksdb-project/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d315341d0124e2b615",
+    "url": "/react-drinksdb-project/static/js/main.1916ac60.chunk.js"
+  },
+  {
+    "revision": "54c00450c0785ff2be23",
+    "url": "/react-drinksdb-project/static/js/runtime-main.78b02717.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/react-drinksdb-project/static/media/logo.9a3d2645.svg"
+  }
+]);
