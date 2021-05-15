58dd221ddc786fccd22ab374fa10544c5580b057
RoshRoslin <RoshRoslin@gmail.com>
2021-05-15 14:56:39 -0700

    Production build source code, first deploy

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        libero, dignissimos dolore sint eaque accusantium corporis enim
+        assumenda in doloribus doloremque veritatis illo reiciendis sunt? Non
+        dolorum at delectus ratione.
+      </p>
+    </section>
+  );
+};
+
+export default About;
